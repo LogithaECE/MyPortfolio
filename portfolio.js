0efc1c89f92e514486cd85d91c6e9d83625aa634
LogithaECE <125351551+LogithaECE@users.noreply.github.com>
2024-03-17 16:45:45 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+function learn(){
+    window.location.href = "learn.html";
+}
+function pet(){
+    window.location.href = "https://github.com/LogithaECE/Pet-Adoption-system";
+}
+function train(){
+    window.location.href = "https://github.com/LogithaECE/Railway-ticket-generation";
+}
+function resume(){
+    window.location.href = "https://drive.google.com/file/d/1s7ZC6FS1M4Q7dXKvijCqVziODsMJMFlF/view?usp=drive_link";
+}
+function certificate(){
+    window.location.href = "https://drive.google.com/drive/folders/1hhcQPAHMZevXrsy7ZjR6Ka00tFqqPFLP";
+}
