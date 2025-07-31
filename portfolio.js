cb6384401a85ea22398138c738dec3127f7932da
LogithaECE <125351551+LogithaECE@users.noreply.github.com>
2025-07-31 23:05:31 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+function resume() {
+  window.location.href =
+    "https://drive.google.com/file/d/1gkvOKatfknGB95ZEs6gLFdMM3rcse5PU/view?usp=sharing";
+}
+function certificate() {
+  window.location.href =
+    "https://drive.google.com/drive/folders/1hhcQPAHMZevXrsy7ZjR6Ka00tFqqPFLP";
+}
