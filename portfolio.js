fc334f160fd281eaf76a34de81be4971d1267775
LogithaECE <125351551+LogithaECE@users.noreply.github.com>
2026-05-08 12:00:13 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,6 @@
 function resume() {
   window.location.href =
-    "https://drive.google.com/file/d/1gkvOKatfknGB95ZEs6gLFdMM3rcse5PU/view?usp=sharing";
+    "https://drive.google.com/file/d/1WEN4raHlyAhDotq5Y-H3Ekpriixp1G5A/view?usp=sharing";
 }
 function certificate() {
   window.location.href =
